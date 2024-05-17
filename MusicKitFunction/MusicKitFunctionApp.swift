06d399d5238d58913b56d5db09a4341ab98f79bb
HikaruHonda <hikaru.h.developer@gmail.com>
2024-05-17 20:46:45 +0900

    Initial Commit

---
+++ MusicKitFunction/MusicKitFunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicKitFunctionApp.swift
+//  MusicKitFunction
+//
+//  Created by 本田輝 on 2024/05/17.
+//
+
+import SwiftUI
+
+@main
+struct MusicKitFunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
